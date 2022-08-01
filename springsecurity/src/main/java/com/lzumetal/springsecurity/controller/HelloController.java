539098b579c00af8c43d1252b4dc4e2f1c363ab2
liaosi <liaosilzu2007@163.com>
2022-08-01 22:39:12 +0800

    Initial commit

---
+++ springsecurity/src/main/java/com/lzumetal/springsecurity/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.lzumetal.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liaosi
+ * @date 2022-08-01
+ */
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
